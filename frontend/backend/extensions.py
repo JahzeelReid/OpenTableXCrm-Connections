387c2707ba3f813f0146dc2a564b780ed35d041a
Jahzeel Reid <reidJahzeel@gmail.com>
2026-01-20 23:43:25 -0500

    1st

--- frontend/backend/extensions.py
+++ frontend/backend/extensions.py
@@ -6,4 +6,8 @@ class Base(DeclarativeBase):
     pass
 
 
-db = SQLAlchemy(model_class=Base)
+# db = SQLAlchemy(model_class=Base)
+
+engine_options = {"pool_pre_ping": True}
+
+db = SQLAlchemy(model_class=Base, engine_options=engine_options)
